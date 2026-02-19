85f6dd65c4e89af1e9f0aee99b729ac5451fbfd3
Pryce Tharpe <pjtharpe04@gmail.com>
2026-02-19 17:21:33 -0500

    ts error resolved

--- api/config.ts
+++ api/config.ts
@@ -1,3 +1,5 @@
+declare const process: { env: Record<string, string | undefined> };
+
 export const config = { runtime: "edge" };
 
 export default async function handler(request: Request): Promise<Response> {
